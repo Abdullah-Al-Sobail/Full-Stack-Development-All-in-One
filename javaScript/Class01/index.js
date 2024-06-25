d47e2b2d75a68170e17a4767acf104ca08a8a398
Abdullah Al Sobail <abdullahalsobail78@gmail.com>
2024-06-25 22:49:28 +0600

    JS Class

---
+++ javaScript/Class01/index.js
@@ -0,0 +1,15 @@
+// document.write(10);
+// window.alert('Hi i am alret!');
+/*This is
+multi line
+comment*/
+// console.log('Hi i am console');
+// var x=5;
+// const y=7;
+// document.write(x+"</br>");
+// document.write(y);
+// A-Z,a-z,0-9,_,$
+var frist_name='Sakib';
+var _Fristname='Sakib';
+var Fristname1='Sakib';
+var fristName
---
+++ javaScript/Class02/index.js
@@ -0,0 +1,31 @@
+ 
+//  let x=5;
+
+// console.log(x);
+// let x1=7;
+// {
+//     let x=6;
+//     let x=7;
+
+//     // console.log(x);
+// }
+// console.log(x);
+// const x=5;
+// {
+
+//     const x=7;
+// }
+
+// console.log(x);
+// let x=5;
+// let y=8;
+// let y='7';
+// let w=9;
+// let sum=x+z+y+w;
+// let result=y%x;
+// console.log(result);
+// let x=20;
+// x*=3;
+// console.log(x);
+x=5+7;
+console.log(x);
