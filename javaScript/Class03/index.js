a3158e330d2aeaec9ac87f190d149c7a288875fd
Abdullah Al Sobail <abdullahalsobail78@gmail.com>
2024-06-26 22:54:25 +0600

    Class 03

---
+++ javaScript/Class03/index.js
@@ -0,0 +1,34 @@
+// let x=test(4,5)
+// // let y=test(10,20);
+
+// function test(num1,num2){
+// // console.log('End claculation');
+//  return num1*num2;   
+ 
+// }
+// console.log('The result of the calculation is : '+x);
+// const studentInfo={
+//     fristName:'Jhon',
+//     age:30,
+//     gender:'Male',
+//     language:{native:'Bangla',secondLag:"English"},
+//     lastName:'Doe',
+//     fullName:function(){
+//         return 'Full name is '+this.fristName+" "+this.lastName;
+//     }
+// }
+// console.log(studentInfo.language.secondLag);
+//Create an Object
+const person = {
+    firstName:"John",
+    lastName:"Doe",
+    age:50, eyeColor:"blue"
+  }
+  
+  // Create a copy
+  const x = person;
+  
+  // Change Age in both
+//   x.age = 10;
+x.age=10;
+console.log(person);
