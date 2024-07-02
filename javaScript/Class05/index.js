c9fbae2418c222af054c6896c57895b9380c3f6f
Abdullah Al Sobail <abdullahalsobail78@gmail.com>
2024-07-02 23:34:16 +0600

    Class05

---
+++ javaScript/Class05/index.js
@@ -0,0 +1,23 @@
+// function displayDate(){
+
+
+
+//     console.log(Date());
+// }
+// let text1=`thsi is p
+// Element in html
+
+// `
+
+// let text2="it's text2";
+// // let text3=`it's the combination of "text1 and text2"`;
+// console.log(text1);
+// console.log(document);
+// let text=new String("Simple variable");
+
+// console.log(typeof text);
+// let x = new String("John");
+// let y = new String("John");
+// let text = "HELLO WORLD";
+// text[0] = "A";
+// console.log(text)
