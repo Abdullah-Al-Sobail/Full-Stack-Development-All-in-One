ee59ad63e856cbfdd4342bfc107effba926a0cf6
Abdullah Al Sobail <abdullahalsobail78@gmail.com>
2024-07-03 23:15:51 +0600

    Class 06

---
+++ javaScript/Class06/index.js
@@ -0,0 +1,32 @@
+
+// let y=x.substring(0);
+// let y=x.substr(3);
+// console.log(y.toUpperCase());
+// console.log(y.toLowerCase());
+// console.log(x);
+// let x='         Hello World         ';
+// let y='This is 2nd String';
+// console.log(x+y);
+// let z=x.concat('-',y);
+// let y=x.trim()
+// console.log(x.length);
+// console.log(y.length);
+// let text = "5";
+// let padded = text.padStart(5,"*");
+// let z=y.substr(3,100)
+// let k=z.padStart(100,'*')
+// let z=10;
+// let k=z.toString();
+// let padded=k.padStart(10,'*');
+// let text='Hello, World  World';
+// let repeat=text.repeat(10);
+// let replace=text.replaceAll('World','JavaScript');
+// let split=text.split(',');
+// console.log(text);
+// console.dir(text.indexOf('World',8));
+// let text = "The rain in SPAIN stays mainly in the plain";
+// console.log(text.matchAll('in'));
+// let name='Sakib';
+// let age=30;
+// document.write(`<b>Our new student name 
+//     is</b> :${name} and <u>his age is </u>: ${age}`);
