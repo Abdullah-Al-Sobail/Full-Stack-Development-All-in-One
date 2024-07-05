c250162c68a028025b91659aaaedc4f7dbd53e46
Abdullah Al Sobail <abdullahalsobail78@gmail.com>
2024-07-05 23:40:47 +0600

    Class 07

---
+++ javaScript/Class07/index.js
@@ -0,0 +1,31 @@
+// let x=(0.2*10+0.1*10)/10;
+// console.log(y);
+// let x=5;
+// let z=30
+// let y='10';
+// let z="The result is : "+x+y;
+// console.log(x+z+y+20);
+// let x=100;
+// let y=0;
+// console.log(typeof Infinity);
+// let myNumber = 32;
+// console.log(myNumber.toString(32));
+// console.log(myNumber.toString(16));
+// console.log(myNumber.toString(12));
+// console.log(myNumber.toString(10));
+// console.log(myNumber.toString(8));
+// console.log(myNumber.toString(2));
+// let x =5n;
+// let studentInfo={
+//     name:'Sakib',
+//     age:30
+// }
+let y =20;
+let x=y.toString();
+// console.log(typeof x);
+// console.log(Number(x)*y);
+console.log(parseFloat(y));
+// console.log(typeof x);
+// console.log(studentInfo.name);
+
+
