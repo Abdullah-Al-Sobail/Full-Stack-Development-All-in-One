2eaebac48c1d3b892ed09900f6f19afe2620ca8d
Abdullah Al Sobail <abdullahalsobail78@gmail.com>
2024-07-09 00:28:23 +0600

    Class08

---
+++ javaScript/Class08/index.js
@@ -0,0 +1,67 @@
+// console.log(new Number);
+// const person={
+//     name:'Sakib',
+//     age:20,
+//     country:'Bangladesh'
+// }
+// console.log(person)
+// console.log(Number.EPSILON);
+// const numbers=[1,2,3];
+// const country=[];
+// console.log(numbers);
+// country[0]='Bangladesh';
+// country[1]='Pakistan';
+// country[3]='India';
+// country[4]='Nepal';
+// console.log(country);
+// const cars = new Array("Saab", "Volvo", "BMW");
+// console.log(cars);
+// const cars = ["Saab", "Volvo", "BMW"];
+// cars[0]='BMW';
+// console.log(cars.toString());
+// console.log(cars);
+// const studentInfo=['Sakib',{
+//     fLang:'Bangla',
+//     sLang:'English'
+// },'Bangladesh'];
+
+// console.log(cars);
+// console.log(cars[cars.length-1]);//cars[3]
+// console.log(cars[0]);
+// console.log(cars[1]);
+// let len=cars.length;
+// for (let i=0; i<len;i++){
+//     console.log(cars[i]);
+// }
+// console.log('End');
+// console.log(cars);
+
+// cars.forEach(myFunction)
+// function myFunction(value1){
+//     console.log(value1);
+// }
+// console.log(cars);
+// cars.push('item1');
+// console.log(cars);
+// const person={
+//     name:'Sakib',
+//     age:30
+// }
+// const cars = ["Saab", "Volvo", "BMW"];
+// cars[5]='item1';
+// console.log(cars);
+// console.log(person);
+// const person1 = [];
+// person1["firstName"] = "John";
+// person1["lastName"] = "Doe";
+// person1["age"] = 46;
+// console.log(person1[0]);
+// const points = [40];
+const cars = ["Saab", "Volvo", "BMW"];
+const person={
+    name:'Sakib',
+    age:30
+}
+// console.log(Array.isArray(cars));
+console.log(cars instanceof Array);
+console.log(person instanceof Array);
