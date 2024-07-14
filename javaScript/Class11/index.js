0f6e33f9af66d860e8e5c57da7461335f10f7281
Abdullah Al Sobail <abdullahalsobail78@gmail.com>
2024-07-15 00:18:22 +0600

    Class 11

---
+++ javaScript/Class11/index.js
@@ -0,0 +1,33 @@
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// console.log(fruits.toSorted());
+// console.log(fruits);
+// console.log(fruits.reverse());
+
+// console.log(points.sort(
+//     function(){
+//         return 0.5-Math.random();//-60 39
+//     }
+// ));
+//const points = [40, 100, 1, 5, 25, 10];
+// console.log(points.sort(
+//     function(a,b){
+//         return a-b;
+//     }
+// ))
+// console.log(points[points.length-1]);
+// console.log(points[0]);
+// let minValue=min(points);
+// function min(arr){
+//     return Math.min.apply(null,arr);
+// }
+// console.log(minValue);
+const cars = [
+    {type:"Volvo", year:2016},
+    {type:"Saab", year:2001},
+    {type:"BMW", year:2010}
+  ];
+console.log(cars.sort(
+    function(a,b){
+        return a.year-b.year;
+}
+)) 
