760ac0989ed8f1aa08386ec36c5ed6d562e883e1
Abdullah Al Sobail <abdullahalsobail78@gmail.com>
2024-08-21 08:17:54 +0600

    Class15-16

---
+++ javaScript/Class15/index.js
@@ -0,0 +1,22 @@
+// let x=5;
+// let y=5;
+// if(x>4 && y>10){
+
+// console.log("Value is true");
+// }
+// console.log("value not found");
+// let age=18;
+// let voteable=(age>18)?"Voteable person":(age==18)?"18 is conditonally applicable":"Too young";
+// console.log(voteable);
+// let age=10;
+// if (isNaN(age)){
+//     console.log("Input is not a number");
+// }else{
+//     console.log((age>=18)?"voteable":"Too Young");
+
+// }
+const studentInfo={
+    age:20,
+    dep:"CSE"
+}
+console.log(studentInfo?.name);
---
+++ javaScript/Class16/index.js
@@ -0,0 +1,39 @@
+// let number=-1;
+// if(number>0){
+//     console.log("The number is greater than zero");
+
+// }
+// else if(number==0){
+//     console.log("This is a neutral number");
+// }else{
+//     console.log("No positive number found");
+
+// }
+let day="0";
+switch (day) {
+    default:
+        day = "Looking forward to the Weekend";
+        break;
+    case 0:
+      day = "Sunday";
+      break;
+    case 1:
+      day = "Monday";
+      break;
+    case 2:
+       day = "Tuesday";
+      break;
+    case 3:
+      day = "Wednesday";
+      break;
+    case 4:
+      day = "Thursday";
+      break;
+    case 5:
+      day = "Friday";
+      break;
+    case 6:
+      day = "Saturday";
+   
+  }
+  console.log(day);
