75b1588182ecb844a5f6fc1249936c6f923e0818
Abdullah Al Sobail <abdullahalsobail78@gmail.com>
2024-09-03 23:45:01 +0600

    Class20

---
+++ javaScript/Class20/index.js
@@ -0,0 +1,33 @@
+
+
+//  console.log(letters.has('w'));
+// for(let x of letters){
+//     console.log(x);
+// }
+// letters.forEach(function(value){
+//     console.log(value);
+// });
+
+// letters.forEach(function (value){
+//     console.log(value);
+// });
+// const letters=new Set(['a','b','c']);
+// let letter=letters.values();
+// for(let x of letter){
+//     console.log(x);
+// }
+// console.log(letters.keys());
+// console.log(letters.entries());
+// const info={
+//     name:"Jhon",
+    
+// }
+// for(let x in info){
+//     console.log(typeof x);
+// }
+const fruits = new Map([
+    ["apples", 500],
+    ["bananas", 300],
+    ["oranges", 200]
+  ]);
+  console.log(fruits instanceof Map);
