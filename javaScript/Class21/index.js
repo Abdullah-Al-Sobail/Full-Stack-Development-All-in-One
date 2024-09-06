caac14da076191300f8de7ee2d2f2349d316dfd1
Abdullah Al Sobail <abdullahalsobail78@gmail.com>
2024-09-06 23:28:10 +0600

    Class21

---
+++ javaScript/Class21/index.js
@@ -0,0 +1,29 @@
+// const fruits = new Map([
+//     ["apple",500],
+//     ["bannnas",300]
+// ]);
+// console.log(fruits.keys())
+// console.log(fruits.get("bananas"))
+// fruits.set('apple',500);
+// fruits.set('bannas',300);
+// fruits.delete('bannas');
+// // fruits.clear();
+// console.log(fruits.has('bannas'));
+// fruits.forEach(function(value,key){
+//     console.log(`${key}:${value}`);
+// });
+// console.log(fruits.entries());
+// for(let x of fruits.entries()){
+//     console.log(x);
+//     console.log(`${x[0]}:${x[1]}`);
+// }
+// const apple={name:'Apple'};
+// const bannas={name:'Bannas'};
+// const fruits=new Map();
+// fruits.set(apple,500);
+// fruits.set(bannas,300);
+// console.log(fruits);
+let name=true;
+console.log(typeof Symbol());
+console.log(typeof age);
+console.log(typeof null);
