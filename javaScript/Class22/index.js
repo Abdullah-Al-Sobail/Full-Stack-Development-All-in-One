6c867acfcb95922562af1964cafa51bda6b5db10
Abdullah Al Sobail <abdullahalsobail78@gmail.com>
2024-09-08 23:53:14 +0600

    Class22

---
+++ javaScript/Class22/index.js
@@ -0,0 +1,26 @@
+// let hour =5;
+// // if(hour<18){
+// //     console.log('Hello');
+// // }else{
+// //     console.log("False");
+// // }
+// let result= hour<18?console.log('Hello'):console.log('Hi');
+// const obj={
+//     name:'Jhon',
+//     age:20
+// }
+// // console.log(obj.constructor);
+// // let date=new Date();
+// // console.log(date.constructor);
+// // let number=[1,2,4,5,7,8];
+// // console.log(number.constructor);
+// console.log(typeof typeof obj);
+// let x=8.65147;
+// let y="Jhon";
+// let z=String(100+20);
+// // let result=x-y;
+// console.log(parseInt(x));
+// console.log(parseFloat(x));
+// console.log(x.toFixed(3));
+// console.log(x.toPrecision(4));
+console.log(typeof 8);
