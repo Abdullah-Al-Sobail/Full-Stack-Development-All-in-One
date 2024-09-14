d2c5082ebbb1de6df3acc42f8ebcf98b9e464cf9
Abdullah Al Sobail <abdullahalsobail78@gmail.com>
2024-09-14 23:56:24 +0600

    Class24

---
+++ javaScript/Class24/index.js
@@ -0,0 +1,15 @@
+// let text="Is this all there is?";
+// let position=text.search(/w3Schools/i);
+// let replace=text.replace(/microsoft/i,"w3Schools");
+// console.log(text);
+// let search=text.match(/is/ig);
+// console.log(search);
+// console.log(typeof /w/.exec("The best things in life are free!"));
+// let x = 100 + 50 * 3;
+// console.log(x);
+
+try{
+   addalret("hello");
+}catch(err){
+    console.log("The error is :"+err.message);
+}
